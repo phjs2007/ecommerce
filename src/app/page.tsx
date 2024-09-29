c384b6cb944e0fa20320d84575f0bbbdb151000b
phjs2007 <phjs7626@gmail.com>
2024-09-29 00:59:36 -0300

    3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,11 @@
 import Image from "next/image";
 import Frost from "./components/frost";
 import { useEffect, useState } from "react";
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-
-import './styles.css';
-
-// import required modules
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
-
-
-
 
 export default function Home() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true); // Estado para controlar o carregamento
-  const [error, ] = useState(null); // Estado para controlar erros
-
+  const [error, setError] = useState(null); // Estado para controlar erros
 
   useEffect(() => {
     const fetchData = async () => {
@@ -45,64 +30,44 @@ export default function Home() {
   if (error) return <p>{error}</p>; // Mensagem de erro
 
   return (
-    <div className="relative bg-[url('/imagens/2.jpg')] box-border w-full h-[50vh] object-cover ">
+    <div className="relative bg-[url('/imagens/2.jpg')] box-border w-full h-[50vh] object-cover">
       <main className="box-border w-screen h-screen p-main relative z-20">
         <section>
           <Frost />
         </section>
-        <section className=" w-full flex overflow-x-auto  gap-1 mt-6 ">
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper"
-      >
+        <section className="w-full flex overflow-x-auto gap-1 mt-6">
           {data.map((game) => {
             const { id, name, nota, descricao_breve, price, imageUrl } = game; // Incluindo imageUrl
             return (
-              <SwiperSlide key={id} className="flex gap-4 mx-3">
-                  <div className="w-full">
-                  <Image
+              <div key={id} className="group rounded-lg flex-none w-1/4 h-fit p-2 hover:scale-125">
+                <div className="group-hover:">
+                  {imageUrl && (
+                    <img
                       src={imageUrl}
                       alt={name}
-                      width={500} // largura em pixels
-                      height={200} // altura em pixels
-                      className="w-full h-auto"
-                  />
-                  </div>
-                  <div className="flex flex-col justify-center p-2 gap-3 bg-slate-300">
-                    <div className="flex justify-between">
-                    <h1>
-                      {name}
-                    </h1>
+                      width={500}
+                      height={100}
+                      className="object-fill" // Estilo responsivo
+                    />
+                  )}
+                </div>
+                <div className="flex flex-col space-y-8 bg-slate-700 text-gray-50 p-3 group-hover:text-xs">
+                  <div className="flex justify-between group-hover:text-xs">
+                    <h1 className="group-hover:text-xs">{name}</h1>
                     <div>
-                      <p>nota: {nota}</p>
-                    </div>
-                    </div>
-                    <div>
-                      <p>
-                        {descricao_breve}
-                      </p>
-                    </div>
-                    <div className="flex justify-end">
-                      <p>
-                        R${price}
-                      </p>
+                      <p>Nota: {nota}</p>
                     </div>
                   </div>
-
-              </SwiperSlide>
+                  <div>
+                    <p>{descricao_breve}</p> {/* Exibe a descrição breve */}
+                  </div>
+                  <div className="flex justify-end mr-2 group-hover:text-sm">
+                    <p>R${price}</p>
+                  </div>
+                </div>
+              </div>
             );
           })}
-          </Swiper>
-          
         </section>
       </main>
     </div>
